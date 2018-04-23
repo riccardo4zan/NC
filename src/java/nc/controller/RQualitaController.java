c6dd6689f9caf8cb68b92add5970546db990ad02
como <como@finn>
2018-04-23 21:28:48 +0200

    RC pagina

--- src/java/nc/controller/RQualitaController.java
+++ src/java/nc/controller/RQualitaController.java
@@ -1,11 +1,18 @@
 package nc.controller;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import nc.model.Fornitore;
 import nc.model.NonConformita;
+import nc.model.Reparto;
+import nc.model.Tipo;
 import nc.model.User;
 import nc.service.DipendenteService;
 import nc.service.NonConformitaService;
 import nc.service.TipoService;
 import nc.service.UserService;
+import nc.utility.ChartData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,8 +42,62 @@ public class RQualitaController {
     @RequestMapping(value = {"/", ""}, method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView model = new ModelAndView();
+        int anno = Calendar.getInstance().get(Calendar.YEAR);
 
-        model.setViewName("indexRQualita");
+        //inserire qui elaborazioni aperte
+        model.addObject("matricola", MainController.getLoggedDip().getMatricola());
+
+        //dati thumbnais
+        model.addObject("ncAnno", ncs.findNumeroNCAnno(anno));
+        
+        
+        // prendo tutti i tipi, i reparti e i fornitori presenti
+        List<Reparto> reparti = rs.findAll();
+        List<Fornitore> fornitori = fs.findAll();
+        List<Tipo> tipi = ts.findAll();
+
+        //ricavo i dati riguardanti tipi, reparti e fornitori presenti
+        ArrayList<ChartData> repartiData = new ArrayList<>();
+        ArrayList<ChartData> fornitoriData = new ArrayList<>();
+        ArrayList<ChartData> tipiData = new ArrayList<>();
+
+        for (Fornitore tmp : fornitori) {
+            double costo = ncs.findCostoPerFornitore(anno, tmp);
+            if (costo != 0) {
+                fornitoriData.add(new ChartData(tmp.getNome(), costo));
+            }
+        }
+        for (Reparto tmp : reparti) {
+            double costo = ncs.findCostoPerReparto(anno, tmp);
+            if (costo != 0) {
+                repartiData.add(new ChartData(tmp.getNome(), costo));
+            }
+        }
+        for (Tipo tmp : tipi) {
+            double costo = ncs.findCostoPerTipo(anno, tmp);
+            if (costo != 0) {
+                tipiData.add(new ChartData(tmp.getNome(), costo));
+            }
+        }
+        model.addObject("repartiData", repartiData);
+        model.addObject("tipiData", tipiData);
+        model.addObject("fornitoriData", fornitoriData);
+
+        // ricavo dati istogramma
+        ArrayList<ChartData> istogramma = new ArrayList<>();
+        String[] mesi = {"", "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"};
+        double max = 0;
+        for (int i = 1; i <= 12; i++) {
+            double costo = ncs.findCostoAnnoPerMese(i, anno);
+            if (costo > max) {
+                max = costo;
+            }
+            istogramma.add(new ChartData(mesi[i], costo));
+        }
+        model.addObject("istogramma", istogramma);
+        model.addObject("max", max);
+
+        model.setViewName("indexManager");
         return model;
     }
 
